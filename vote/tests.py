67cb4a3887093140ef4a4ddd65138489188e6a38
classbinu <classbinu@gmail.com>
2022-07-16 17:37:00 +0900

    Make test for create vote

--- vote/tests.py
+++ vote/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework.views import status
+from .models import Vote
 
-# Create your tests here.
+
+class VoteCrudTest(APITestCase):
+
+    def setUp(self):
+        self.url = reverse('vote:api-list')
+
+    def test_create_vote(self):
+        url = reverse('vote:api-list')
+        data = {
+            'subject': '테스트 투표를 생성합니다',
+            'password': '123456',
+        }
+        response = self.client.post(url, data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        obj = Vote.objects.get()
+        self.assertEqual(data['subject'], obj.subject)
+        self.assertEqual(data['password'], obj.password)
+
+    def test_read_vote(self):
+        pass
+
+    def test_update_vote(self):
+        pass
+
+    def test_delete_vote(self):
+        pass
--- vote/urls.py
+++ vote/urls.py
@@ -3,7 +3,9 @@ from rest_framework import routers
 
 from .views import VoteViewSet
 
+app_name = 'vote'
+
 router = routers.SimpleRouter()
-router.register('vote', VoteViewSet)
+router.register('vote', VoteViewSet, basename='api')
 
 urlpatterns = router.urls
